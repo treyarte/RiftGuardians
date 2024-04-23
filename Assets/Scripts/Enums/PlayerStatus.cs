ffb824f8f35388efc453361825393775ff0c1af3
Treyvion Arte Johnson <tajohns22@gmail.com>
2024-04-22 22:56:43 -0500

    first commit

---
+++ Assets/Scripts/Enums/PlayerStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Script.Enums
+{
+    /// <summary>
+    /// Status of the player
+    /// </summary>
+    public enum PlayerStatus
+    {
+        Unknown = 0,
+        Alive = 1,
+        Dead = 444,
+        
+    }
+}
