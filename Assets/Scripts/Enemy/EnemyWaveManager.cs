906dbc94b15aaafdb63ad9fd0f9cd48d2c433a16
Treyvion Arte Johnson <tajohns22@gmail.com>
2024-06-16 22:20:05 -0500

    adding wave ui

--- Assets/Scripts/Enemy/EnemyWaveManager.cs
+++ Assets/Scripts/Enemy/EnemyWaveManager.cs
@@ -8,12 +8,16 @@ using UnityEngine;
 public class EnemyWaveManager : MonoBehaviour
 {
     
-    [SerializeField] private EnemyWave[] _waves;
+    [SerializeField] public EnemyWave[] _waves;
     [SerializeField] private SplineComputer _mainSpline;
     [SerializeField] private bool _canSpawnEnemy = true; 
     private EnemyWave _currentWave { get; set; }
+    [SerializeField]
+    private int _currentWaveNum = 0;
     public GameObject[] currentEnemies;
 
+    public static event Action<int> WavesCompleted; 
+    
     private void OnEnable()
     {
         PathTriggers.StartPositionCheck += UpdateCanSpawn;
@@ -23,22 +27,24 @@ public class EnemyWaveManager : MonoBehaviour
     {
         PathTriggers.StartPositionCheck -= UpdateCanSpawn;
     }
-
+    
     private void Awake()
     {
         foreach (var currWave in _waves)
         {
+            StartCoroutine(SpawnWave(currWave));
+  
         }
 
-        _currentWave = _waves.First();
-        StartCoroutine(SpawnWave(_waves.First()));
+        // _currentWave = _waves.First();
     }
 
     private IEnumerator SpawnWave(EnemyWave currWave)
     {
         
         // _currentWave = currWave;
-        _currentWave.hasStarted = true;
+        // _currentWave.hasStarted = true;
+        currWave.hasStarted = true;
         StartCoroutine(WaitToSpawnEnemy());
         var index = 0;
         foreach (var enemy in currWave.enemies)
@@ -64,7 +70,9 @@ public class EnemyWaveManager : MonoBehaviour
             yield return new WaitUntil(() => _canSpawnEnemy);
             yield return new WaitForSeconds(5f);
         }
-
+        //Enemies need to be defeated
+        _currentWaveNum++;
+        WavesCompleted.Invoke(_currentWaveNum);     
         // yield return new WaitForSeconds(currWave.duration);
     }
 
